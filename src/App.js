a4d494da1a622d95af94282749a02189db811f5d
aminalakhsham <aminking140@gmail.com>
2019-04-04 22:19:52 +0300

    add fake data

--- src/App.js
+++ src/App.js
@@ -3,6 +3,24 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "./components/Navbar";
 
 class App extends Component {
+  state = {
+    watchList: [
+      {
+        Name: "CAPTAIN MARVEL",
+        img:
+          "https://image.tmdb.org/t/p/w600_and_h900_bestv2/AtsgWhDnHTq68L0lLsUrCnM7TjG.jpg",
+        date: "4April,2019",
+        status: false
+      },
+      {
+        Name: "CAPTAIN MARVEL",
+        img:
+          "https://image.tmdb.org/t/p/w600_and_h900_bestv2/AtsgWhDnHTq68L0lLsUrCnM7TjG.jpg",
+        date: "4April,2019",
+        status: true
+      }
+    ]
+  };
   render() {
     return (
       <Router>
