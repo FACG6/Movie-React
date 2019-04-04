e537103a9276b46d6baf2b98b918ead54d789e7f
alaabadra <alaabadra4@gmail.com>
2019-04-04 16:57:04 +0300

    create folder Header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+const Header = props => (
+<div className="container">
+    <div className="sectionSearch">
+            <button className="btnSearch">Search</button>
+    </div>
+</div>    
+)
+export default Header;
